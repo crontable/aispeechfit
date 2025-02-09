da2d68bd1e71c944c99528fc2e9a56334628e6e5
Hojoong Choi <crontables@gmail.com>
2025-02-09 15:32:22 +0900

    fix: git conflict for rebase

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import { Message } from '@/components/form-message';
 import { Button } from '@/components/ui/button';
 import { createClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
+  const router = useRouter();
   const supabase = createClient();
 
   async function signInWithKakao() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'kakao',
     });
+
+    if (data.url) {
+      router.push(data.url);
+    }
+
     console.log('kakao', data, error);
   }
 
