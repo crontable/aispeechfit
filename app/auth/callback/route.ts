cdbd80183883e4f19594bd7cfa16743c3cc3d789
Hojoong Choi <crontables@gmail.com>
2025-04-20 14:41:22 +0900

    fix: [FE] 로그인 라우팅 접근 제어

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,26 +1,28 @@
 import { NextResponse } from 'next/server';
 // The client you created from the Server-Side Auth instructions
 import { createClient } from '@/utils/supabase/server';
+import { ROUTE_PATH } from '@/app/routes';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/';
+  const next = searchParams.get('next') ?? ROUTE_PATH.STUDY;
 
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development';
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(new URL(`${origin}${next}`, request.url));
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        return NextResponse.redirect(new URL(`https://${forwardedHost}${next}`, request.url));
       } else {
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(new URL(`${origin}${next}`, request.url));
       }
     }
   }
